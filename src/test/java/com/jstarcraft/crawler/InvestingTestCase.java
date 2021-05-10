d1ff2e2cca65797bca4b78fc679b46ed27c84cbd
Birdy <Birdy@LAPTOP-R44M770M>
2021-05-10 16:02:06 +0800

    股价接口.

---
+++ src/test/java/com/jstarcraft/crawler/InvestingTestCase.java
@@ -0,0 +1,82 @@
+package com.jstarcraft.crawler;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 英为单元测试
+ * 
+ * @author Birdy
+ *
+ */
+public class InvestingTestCase {
+
+    public static void main(String[] arguments) {
+        final String host = "127.0.0.1";
+        final int port = 1080;
+        HttpHost proxy = new HttpHost(host, port);
+        HttpClientBuilder builder = HttpClientBuilder.create();
+        builder.setProxy(proxy);
+        HttpClient client = builder.build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setHttpClient(client);
+        try {
+            RestTemplate template = new RestTemplate(factory);
+            // 拦截器
+            template.getInterceptors().add(new ClientHttpRequestInterceptor() {
+
+                @Override
+                public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+                    ClientHttpResponse response = execution.execute(request, body);
+                    return response;
+                }
+
+            });
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+            headers.add(HttpHeaders.ACCEPT, "text/plain, */*; q=0.01");
+            headers.add(HttpHeaders.ACCEPT_LANGUAGE, "zh-CN,zh;q=0.9,en;q=0.8");
+            headers.add(HttpHeaders.CACHE_CONTROL, "no-cache");
+            headers.add(HttpHeaders.ORIGIN, "https://cn.investing.com");
+            headers.add(HttpHeaders.REFERER, "https://cn.investing.com/equities/");
+            headers.add(HttpHeaders.USER_AGENT, "PostmanRuntime/7.28.0");
+            headers.add("x-requested-with", "XMLHttpRequest");
+            MultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
+            parameters.add("curr_id", "944220");
+            parameters.add("smlID", "1550189");
+            parameters.add("header", "000651历史数据");
+            parameters.add("st_date", "2021/04/15");
+            parameters.add("end_date", "2021/04/20");
+            parameters.add("interval_sec", "Daily");
+            parameters.add("sort_col", "date");
+            parameters.add("sort_ord", "DESC");
+            parameters.add("action", "historical_data");
+            HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(parameters, headers);
+            // 通过英为股票接口获取历史股价
+            ResponseEntity<String> response = template.exchange("https://cn.investing.com/instruments/HistoricalDataAjax", HttpMethod.POST, request, String.class);
+            String content = response.getBody();
+            System.out.println(content);
+        } catch (HttpStatusCodeException exception) {
+            System.out.println(exception.getStatusCode());
+            System.out.println(exception.getResponseBodyAsString());
+        }
+
+    }
+
+}
---
+++ src/test/java/com/jstarcraft/crawler/XueQiuTestCase.java
@@ -0,0 +1,43 @@
+package com.jstarcraft.crawler;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 雪球单元测试
+ * 
+ * @author Birdy
+ *
+ */
+public class XueQiuTestCase {
+
+    public static void main(String[] arguments) {
+        RestTemplate template = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Void> request = new HttpEntity<>((Void) null, headers);
+        ResponseEntity<String> response = template.exchange("https://xueqiu.com/", HttpMethod.GET, request, String.class);
+        List<String> cookies = response.getHeaders().get(HttpHeaders.SET_COOKIE);
+        System.out.println(cookies);
+        String token = null;
+        for (String cookie : cookies) {
+            if (cookie.startsWith("xq_a_token")) {
+                token = cookie;
+                break;
+            }
+        }
+        headers = new HttpHeaders();
+        headers.put(HttpHeaders.COOKIE, Arrays.asList(token));
+        request = new HttpEntity<>((Void) null, headers);
+        // 通过雪球股票接口获取实时股价
+        response = template.exchange("https://stock.xueqiu.com/v5/stock/quote.json?symbol=SZ000651&extend=detail", HttpMethod.GET, request, String.class);
+        String content = response.getBody();
+        System.out.println(content);
+    }
+
+}
