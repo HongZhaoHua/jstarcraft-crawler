1851febff6269612e861a0da023a771ea1976e01
Birdy <Birdy@LAPTOP-R44M770M>
2022-03-07 12:53:33 +0800

    亿牛单元测试重构.

--- src/test/java/com/jstarcraft/crawler/EniuTestCase.java
+++ src/test/java/com/jstarcraft/crawler/EniuTestCase.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import com.jstarcraft.core.common.conversion.json.JsonUtility;
+
 /**
  * 亿牛单元测试
  * 
@@ -19,22 +21,20 @@ import org.springframework.web.client.RestTemplate;
 public class EniuTestCase {
 
     // 行业
-    // https://eniu.com/industry/{industry}/market/sh
-    // https://eniu.com/industry/{industry}/market/sh/json/true
+    // https://eniu.com/industry/{industry}/market/{share}
+    // https://eniu.com/industry/{industry}/market/{share}/json/true
+    // A股sh
+    // B股sz
+    // H股hk
     @Test
     public void testIndustry() throws InterruptedException {
         RestTemplate template = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(null, headers);
-        ResponseEntity<String> response = template.exchange("https://eniu.com/industry/石油行业/market/sh/json/true", HttpMethod.GET, request, String.class);
+        ResponseEntity<String> response = template.exchange("https://eniu.com/industry/银行/market/sh/json/true", HttpMethod.GET, request, String.class);
         String content = response.getBody();
         System.out.println(content.length());
-        System.out.println(content);
-
-        response = template.exchange("https://eniu.com/industry/石油行业/market/sh", HttpMethod.GET, request, String.class);
-        content = response.getBody();
-        System.out.println(content.length());
-        System.out.println(content);
+        System.out.println(JsonUtility.pretty(content));
     }
 
     // 股票
@@ -44,12 +44,30 @@ public class EniuTestCase {
     // 市净率:https://eniu.com/chart/pba/{code}/t/all
     // 市销率:https://eniu.com/chart/psa/{code}/t/all
     // 股息率:https://eniu.com/chart/dva/{code}/t/all
-    // 派息率:https://eniu.com/chart/pxla/{code}
     // ROE/ROA:https://eniu.com/chart/roea/{code}/q/0
+    // 净利润:https://eniu.com/chart/profita/{code}/q/0
+    // 现金流:https://eniu.com/chart/cashflowa/{code}/q/0
+    // 毛利率:https://eniu.com/chart/grossprofitmargina/{code}/q/0
+    // 派息率:https://eniu.com/chart/pxla/{code}
     @Test
-    public void test() {
-
+    public void testStock() {
+        RestTemplate template = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = template.exchange("https://eniu.com/chart/pricea/sh600000/t/12", HttpMethod.GET, request, String.class);
+        String content = response.getBody();
+        System.out.println(content.length());
+        System.out.println(content);
     }
+
+    // 股票
+    // https://eniu.com/gu/{code}
+    // 价格:https://eniu.com/chart/priceh/{code}
+    // 市盈率:https://eniu.com/chart/peh/{code}
+    // 市净率:https://eniu.com/chart/pbh/{code}
+    // 股息率:https://eniu.com/chart/dvh/{code}
+    // ROE/ROA:https://eniu.com/chart/roeh/{code}
+    // 市值:https://eniu.com/chart/marketvalueh/{code}
 
     // 列表
     // https://eniu.com/static/data/stock_list.json
