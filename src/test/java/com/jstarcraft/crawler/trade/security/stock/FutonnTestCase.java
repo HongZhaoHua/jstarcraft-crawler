4feb80291c084882aeab9bc291e40d166feefee0
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-03-19 01:56:35 +0800

    支持获取ISIN代码.

--- src/test/java/com/jstarcraft/crawler/trade/security/stock/FutonnTestCase.java
+++ src/test/java/com/jstarcraft/crawler/trade/security/stock/FutonnTestCase.java
@@ -1,6 +1,15 @@
 package com.jstarcraft.crawler.trade.security.stock;
 
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import com.jstarcraft.core.common.conversion.xml.XmlUtility;
+import com.jstarcraft.core.utility.StringUtility;
 
 /**
  * 富途牛牛单元测试
@@ -15,7 +24,14 @@ public class FutonnTestCase {
     // ISIN:https://www.futunn.com/stock/{code}-HK/company-profile
     @Test
     public void testIsin() {
-
+        RestTemplate template = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(null, headers);
+        String url = StringUtility.format("https://www.futunn.com/stock/{}-HK/company-profile", "00700");
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+        String content = response.getBody();
+        System.out.println(content.length());
+        System.out.println(XmlUtility.prettyHtml(content));
     }
 
 }
--- src/test/java/com/jstarcraft/crawler/trade/security/stock/QianzhanTestCase.java
+++ src/test/java/com/jstarcraft/crawler/trade/security/stock/QianzhanTestCase.java
@@ -1,6 +1,15 @@
 package com.jstarcraft.crawler.trade.security.stock;
 
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import com.jstarcraft.core.common.conversion.xml.XmlUtility;
+import com.jstarcraft.core.utility.StringUtility;
 
 /**
  * 前瞻单元测试
@@ -13,7 +22,14 @@ public class QianzhanTestCase {
     // ISIN:https://xs.qianzhan.com/hs/zhengquan_{code}.{SH/SZ/HK}.html
     @Test
     public void testIsin() {
-
+        RestTemplate template = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(null, headers);
+        String url = StringUtility.format("https://xs.qianzhan.com/hs/zhengquan_{}.{}.html", "600000", "SH");
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+        String content = response.getBody();
+        System.out.println(content.length());
+        System.out.println(XmlUtility.prettyHtml(content));
     }
 
 }
