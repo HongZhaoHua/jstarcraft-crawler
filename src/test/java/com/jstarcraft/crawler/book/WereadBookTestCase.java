144f7c1d7a78858a06970a2eba8e5708a29ba34d
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-03-25 00:22:33 +0800

    微信读书爬虫.

--- src/test/java/com/jstarcraft/crawler/book/WereadBookTestCase.java
+++ src/test/java/com/jstarcraft/crawler/book/WereadBookTestCase.java
@@ -46,6 +46,34 @@ public class WereadBookTestCase {
         }
     }
 
+    /**
+     * 获取某本书的笔记:https://i.weread.qq.com/book/bookmarklist?bookId={}
+     * 获取书籍的热门划线:https://i.weread.qq.com/book/bestbookmarks?bookId={}
+     * 获取书的详情:https://i.weread.qq.com/book/info?bookId={}
+     */
+    @Test
+    public void testI() {
+        try {
+            RestTemplate template = new RestTemplate();
+            File file = new File(WereadBookTestCase.class.getResource("cookie.txt").toURI());
+            String cookie = FileUtils.readFileToString(file, StringUtility.CHARSET);
+            if (cookie.isEmpty()) {
+                throw new RuntimeException("必须填写Cookie才能获取信息");
+            }
+            List<String> cookies = Arrays.asList(cookie);
+            HttpHeaders headers = new HttpHeaders();
+            headers.put(HttpHeaders.COOKIE, cookies);
+            HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(headers);
+            String url = StringUtility.format("https://i.weread.qq.com/book/info?bookId={}", "34261011");
+            ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+            String content = response.getBody();
+            System.out.println(content.length());
+            System.out.println(JsonUtility.prettyJson(content));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
     @Test
     public void testSearch() {
         RestTemplate template = new RestTemplate();
