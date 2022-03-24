f45d77298579e68d8b5f99110e7198f3ecf75270
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-03-24 09:02:12 +0800

    微信读书爬虫.

--- src/test/java/com/jstarcraft/crawler/book/WereadBookTestCase.java
+++ src/test/java/com/jstarcraft/crawler/book/WereadBookTestCase.java
@@ -8,13 +8,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import com.jstarcraft.core.common.conversion.json.JsonUtility;
 import com.jstarcraft.core.common.conversion.xml.XmlUtility;
 import com.jstarcraft.core.utility.StringUtility;
 
 public class WereadBookTestCase {
 
     @Test
-    public void testTag() {
+    public void testSearch() {
+        RestTemplate template = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.USER_AGENT, "PostmanRuntime/7.28.0");
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(null, headers);
+        String url = StringUtility.format("https://weread.qq.com/web/search/global?keyword={}&maxIdx=0&fragmentSize=120&count=20", "9787521721331");
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+        String content = response.getBody();
+        System.out.println(content.length());
+        System.out.println(JsonUtility.prettyJson(content));
+    }
+
+    @Test
+    public void testBook() {
         RestTemplate template = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.USER_AGENT, "PostmanRuntime/7.28.0");
