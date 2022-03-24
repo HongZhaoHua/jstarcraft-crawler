48661dfeee44542d772d94bdcbf40bfa14a1f765
Birdy <Birdy@192.168.2.60>
2022-03-24 20:36:18 +0800

    新华书店.

---
+++ src/test/java/com/jstarcraft/crawler/book/XhsdBookTestCase.java
@@ -0,0 +1,29 @@
+package com.jstarcraft.crawler.book;
+
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import com.jstarcraft.core.common.conversion.xml.XmlUtility;
+import com.jstarcraft.core.utility.StringUtility;
+
+public class XhsdBookTestCase {
+
+    @Test
+    public void testSearch() {
+        RestTemplate template = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.USER_AGENT, "PostmanRuntime/7.28.0");
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(null, headers);
+        String url = StringUtility.format("https://search.xhsd.com/search?keyword={}", "9787545540444");
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+        String content = response.getBody();
+        System.out.println(content.length());
+        System.out.println(XmlUtility.prettyHtml(content));
+    }
+
+}
