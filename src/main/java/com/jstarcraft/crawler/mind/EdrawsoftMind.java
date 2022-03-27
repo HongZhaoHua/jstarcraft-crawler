f37797b5f26b923065d727e045a3395a3da934d3
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-03-27 11:30:08 +0800

    亿图脑图.

---
+++ src/main/java/com/jstarcraft/crawler/mind/EdrawsoftMind.java
@@ -0,0 +1,15 @@
+package com.jstarcraft.crawler.mind;
+
+/**
+ * 亿图脑图
+ * 
+ * https://mm.edrawsoft.cn/wx.html?work_id=137642
+ * https://masterapi.edrawsoft.cn/api/user/0/work/info?work_id=137642
+ * https://edrawcloudpubliccn.oss-cn-shenzhen.aliyuncs.com/work/16066015/2021-6-28/1624880100/outline.json
+ * 
+ * @author Birdy
+ *
+ */
+public class EdrawsoftMind {
+
+}
