4152f2c8539b0b33e8d31cfc942469f37b3be416
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-02 00:56:24 +0800

    掌阅书籍

---
+++ src/main/java/com/jstarcraft/crawler/book/IreaderBook.java
@@ -0,0 +1,11 @@
+package com.jstarcraft.crawler.book;
+
+/**
+ * 掌阅书籍
+ * 
+ * @author Birdy
+ *
+ */
+public class IreaderBook {
+
+}
---
+++ src/test/java/com/jstarcraft/crawler/book/IreaderBookTestCase.java
@@ -0,0 +1,11 @@
+package com.jstarcraft.crawler.book;
+
+/**
+ * https://www.ireader.com.cn/index.php?ca=channel.index&pca=channel.index
+ * 
+ * @author Birdy
+ *
+ */
+public class IreaderBookTestCase {
+
+}
