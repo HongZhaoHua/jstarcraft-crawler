e6cb329c2494125383a13f7c516ab6b28031e61d
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-04 22:39:58 +0800

    和讯债券.

---
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/HexunBond.java
@@ -0,0 +1,14 @@
+package com.jstarcraft.crawler.trade.security.bond;
+
+/**
+ * 和讯债券
+ * 
+ * http://bond.money.hexun.com/data/bond_nationaldebt_list.htm
+ * http://bond.money.hexun.com/data/bond_corpbondissue_list.htm
+ * 
+ * @author Birdy
+ *
+ */
+public class HexunBond {
+
+}
