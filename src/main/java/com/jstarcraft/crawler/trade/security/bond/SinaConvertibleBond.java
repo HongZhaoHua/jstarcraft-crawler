b60b86ea71c71223f6532a58abdbe51874e4cc5a
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-04 22:48:37 +0800

    新浪转债.

--- src/main/java/com/jstarcraft/crawler/trade/security/bond/SinaConvertibleBond.java
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/SinaConvertibleBond.java
@@ -3,9 +3,7 @@ package com.jstarcraft.crawler.trade.security.bond;
 /**
  * 新浪转债
  * 
- * http://data.10jqka.com.cn/ipo/bond/
- * 
- * http://data.10jqka.com.cn/ipo/kzz/
+ * https://vip.stock.finance.sina.com.cn/mkt/#hskzz_z
  * 
  * @author Birdy
  *
