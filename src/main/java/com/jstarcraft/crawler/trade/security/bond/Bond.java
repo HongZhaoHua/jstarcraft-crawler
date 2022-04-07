ef7565e9019aa5b6b1cf69018672e5a1d97f54f3
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-08 00:03:27 +0800

    债券定义.

--- src/main/java/com/jstarcraft/crawler/trade/security/bond/Bond.java
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/Bond.java
@@ -22,4 +22,88 @@ public interface Bond {
      */
     public String getBondName();
 
+    /**
+     * 获取债券面值
+     * 
+     * @return
+     */
+    public float getFaceValue();
+
+    /**
+     * 获取债券价格
+     * 
+     * @return
+     */
+    public float getBondPrice();
+
+    /**
+     * 获取债券涨跌额
+     * 
+     * @return
+     */
+    public float getBondChangeAmount();
+
+    /**
+     * 获取债券涨跌幅
+     * 
+     * @return
+     */
+    public float getBondChangeRate();
+
+    /**
+     * 获取债券开盘价格
+     * 
+     * @return
+     */
+    public float getBondOpeningPrice();
+
+    /**
+     * 获取债券收盘价格
+     * 
+     * @return
+     */
+    public float getBondClosingPrice();
+
+    /**
+     * 获取债券最高价格
+     * 
+     * @return
+     */
+    public float getBondHighestPrice();
+
+    /**
+     * 获取债券最低价格
+     * 
+     * @return
+     */
+    public float getBondLowestPrice();
+
+    /**
+     * 成交额(考虑迁移到Security)
+     * 
+     * @return
+     */
+    public float getTurnover();
+
+    /**
+     * 成交量(考虑迁移到Security)
+     * 
+     * @return
+     */
+    public float getVolume();
+
+    /**
+     * 获取债券净价/干净价格
+     * 
+     * @return
+     */
+    public float getClearPrice();
+
+    /**
+     * 获取债券全价/肮脏价格
+     * 
+     * @return
+     */
+    public float getDirtyPrice();
+
 }
---
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/ShseBond.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.crawler.trade.security.bond;
+
+/**
+ * 上海证券交易所债券
+ * 
+ * 净价与全价换算:http://www.sse.com.cn/market/bonddata/netfull/
+ * 
+ * @author Birdy
+ *
+ */
+public class ShseBond {
+
+}
