b8f0b3667bdbca9aeb0196801e9a1802ddcfaa3b
Birdy <Birdy@192.168.2.60>
2022-04-07 19:44:20 +0800

    英为财情债券.

---
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/InvestingBond.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.crawler.trade.security.bond;
+
+/**
+ * 英为财情债券
+ * 
+ * https://cn.investing.com/rates-bonds/{} 通过SectionInstrument_Ticker获取债券代号
+ * 
+ * @author Birdy
+ *
+ */
+public class InvestingBond {
+
+}
