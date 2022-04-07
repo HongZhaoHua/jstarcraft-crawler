06e4243f7b6846f8c13e38546ea003ee179938a2
Birdy <Birdy@192.168.2.60>
2022-04-07 20:40:20 +0800

    华尔街见闻债券.

---
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/WallstreetcnBond.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.crawler.trade.security.bond;
+
+/**
+ * 华尔街见闻债券
+ * 
+ * https://wallstreetcn.com/markets/bonds
+ * 
+ * @author Birdy
+ *
+ */
+public class WallstreetcnBond {
+
+}
