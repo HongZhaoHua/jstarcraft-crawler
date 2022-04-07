70c8fdf3b42093809445da11b1e88d0f119e7de6
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-08 01:48:39 +0800

    定义转债接口.

--- src/main/java/com/jstarcraft/crawler/trade/security/bond/ConvertibleBond.java
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/ConvertibleBond.java
@@ -5,17 +5,21 @@ import com.jstarcraft.crawler.trade.security.stock.Stock;
 /**
  * 转债
  * 
+ * <pre>
+ * 转债包含债权 / 期权 / 股权
+ * </pre>
+ * 
  * @author Birdy
  *
  */
 public interface ConvertibleBond {
 
     /**
-     * 获取股票
+     * 获取转股股票
      * 
      * @return
      */
-    public Stock getStock();
+    public Stock getConversionStock();
 
     /**
      * 获取转股价格/转换价格
@@ -25,14 +29,22 @@ public interface ConvertibleBond {
     public float getConversionPrice();
 
     /**
-     * 获取转股价值/转换价值(转股价值=转债面值÷转股价格×正股价格)
+     * 获取转股价值/转换价值
+     * 
+     * <pre>
+     * 转股价值 = 转债面值 ÷ 转股价格 × 正股价格
+     * </pre>
      * 
      * @return
      */
     public float getConversionValue();
 
     /**
-     * 获取转股折溢价格(转股溢价=转债价格-转股价值)
+     * 获取转股折溢价格
+     * 
+     * <pre>
+     * 转股溢价 = 转债价格 - 转股价值
+     * </pre>
      * 
      * @return
      */
