1716791b9cd54b4571088c51f50b6daaeb66ff57
Birdy <Birdy@192.168.2.60>
2022-04-08 17:00:23 +0800

    新浪债券.

--- src/main/java/com/jstarcraft/crawler/trade/security/bond/sina/SinaBond.java
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/sina/SinaBond.java
@@ -4,7 +4,8 @@ package com.jstarcraft.crawler.trade.security.bond.sina;
  * 新浪债券
  * 
  * http://finance.sina.com.cn/bond/
- * https://vip.stock.finance.sina.com.cn/mkt/#hs_z
+ * 债券列表页面:https://vip.stock.finance.sina.com.cn/mkt/#hs_z
+ * 债券详情页面:http://money.finance.sina.com.cn/bond/quotes/{code}.html
  * 
  * @author Birdy
  *
--- src/main/java/com/jstarcraft/crawler/trade/security/bond/sina/SinaConvertibleBond.java
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/sina/SinaConvertibleBond.java
@@ -3,7 +3,7 @@ package com.jstarcraft.crawler.trade.security.bond.sina;
 /**
  * 新浪转债
  * 
- * https://vip.stock.finance.sina.com.cn/mkt/#hskzz_z
+ * 转债列表页面:https://vip.stock.finance.sina.com.cn/mkt/#hskzz_z
  * 
  * @author Birdy
  *
