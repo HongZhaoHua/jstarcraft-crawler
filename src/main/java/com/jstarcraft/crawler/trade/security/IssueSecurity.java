40187022dd454e3449f86b3d7863b2a40dc29512
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-09 02:06:46 +0800

    新股新债.

--- src/main/java/com/jstarcraft/crawler/trade/security/IssueSecurity.java
+++ src/main/java/com/jstarcraft/crawler/trade/security/IssueSecurity.java
@@ -11,6 +11,20 @@ import java.time.LocalDate;
 public interface IssueSecurity {
 
     /**
+     * 获取证券代号
+     * 
+     * @return
+     */
+    public String getSecurityCode();
+
+    /**
+     * 获取证券名称
+     * 
+     * @return
+     */
+    public String getSecurityName();
+
+    /**
      * 获取发行日期/申购日期
      * 
      * @return
