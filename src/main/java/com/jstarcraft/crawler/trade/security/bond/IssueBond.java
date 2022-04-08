ff844eacb5f3e629b6452050b0c220b2d8db0852
Birdy <Birdy@192.168.2.60>
2022-04-08 12:33:26 +0800

    新股日期.

---
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/IssueBond.java
@@ -0,0 +1,27 @@
+package com.jstarcraft.crawler.trade.security.bond;
+
+import java.time.LocalDate;
+
+/**
+ * 新债
+ * 
+ * @author Birdy
+ *
+ */
+public interface IssueBond {
+
+    /**
+     * 获取发行日期/申购日期
+     * 
+     * @return
+     */
+    public LocalDate getIssueDate();
+
+    /**
+     * 获取上市日期
+     * 
+     * @return
+     */
+    public LocalDate ListDate();
+
+}
