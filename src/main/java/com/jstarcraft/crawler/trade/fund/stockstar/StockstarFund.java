5b3a629a7be5cd76311055d0971c911736129ee9
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-10 17:02:10 +0800

    证券之星基金.

---
+++ src/main/java/com/jstarcraft/crawler/trade/fund/stockstar/StockstarFund.java
@@ -0,0 +1,17 @@
+package com.jstarcraft.crawler.trade.fund.stockstar;
+
+/**
+ * 证券之星基金
+ * 
+ * 基金列表页面:http://quote.stockstar.com/fund/
+ * 开放式基金列表页面:http://quote.stockstar.com/fund/
+ * 封闭式基金列表页面(折溢价率):http://quote.stockstar.com/fund/
+ * 
+ * 存续期页面:https://fund.eastmoney.com/{code}.html
+ * 
+ * @author Birdy
+ *
+ */
+public class StockstarFund {
+
+}
