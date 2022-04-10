4a05a22a721078d5a822d8f60f0d4a4d700daa5a
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-10 15:22:28 +0800

    基金定义.

---
+++ src/main/java/com/jstarcraft/crawler/trade/fund/EastmoneyFund.java
@@ -0,0 +1,16 @@
+package com.jstarcraft.crawler.trade.fund;
+
+/**
+ * 东方财富基金
+ * 
+ * 基金净值列表页面:http://fund.eastmoney.com/jzzzl.html
+ * 基金估值列表页面:http://fund.eastmoney.com/fundguzhi.html
+ * 基金详情页面:http://fund.eastmoney.com/{code}.html
+ * 基金历史页面:http://fundf10.eastmoney.com/jjjz_{code}.html
+ * 
+ * @author Birdy
+ *
+ */
+public class EastmoneyFund {
+
+}
--- src/main/java/com/jstarcraft/crawler/trade/fund/FunddbFund.java
+++ src/main/java/com/jstarcraft/crawler/trade/fund/FunddbFund.java
@@ -4,6 +4,7 @@ package com.jstarcraft.crawler.trade.fund;
  * 韭圈儿基金
  * 
  * 基金列表页面:https://funddb.cn/fund/index
+ * 基金详情页面:https://funddb.cn/site/fund_details/{code}
  * 
  * @author Birdy
  *
