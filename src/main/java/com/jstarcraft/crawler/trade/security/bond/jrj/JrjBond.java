62ae8d202f68ea8ea1d42a2c2e1759985cf6bbb6
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-10 17:11:20 +0800

    金融界期货和金融界债券.

---
+++ src/main/java/com/jstarcraft/crawler/trade/security/bond/jrj/JrjBond.java
@@ -0,0 +1,14 @@
+package com.jstarcraft.crawler.trade.security.bond.jrj;
+
+/**
+ * 金融界债券
+ * 
+ * 债券列表页面:http://bond.jrj.com.cn/data/
+ * 转债申购页面:http://bond.jrj.com.cn/data/kzz.shtml
+ * 
+ * @author Birdy
+ *
+ */
+public class JrjBond {
+
+}
---
+++ src/main/java/com/jstarcraft/crawler/trade/security/future/jrj/JrjFuture.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.crawler.trade.security.future.jrj;
+
+/**
+ * 金融界期货
+ * 
+ * 期货列表页面:https://summary.jrj.com.cn/futures/{}.shtml
+ * 
+ * @author Birdy
+ *
+ */
+public class JrjFuture {
+
+}
