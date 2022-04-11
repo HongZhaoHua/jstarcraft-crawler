3bb1ef874f89fc651870b97f296560df8e0231fe
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-11 23:58:24 +0800

    指数更新.

--- src/main/java/com/jstarcraft/crawler/trade/index/chinabond/ChinabondBondIndex.java
+++ src/main/java/com/jstarcraft/crawler/trade/index/chinabond/ChinabondBondIndex.java
@@ -8,6 +8,6 @@ package com.jstarcraft.crawler.trade.index.chinabond;
  * @author Birdy
  *
  */
-public class ChinabondIndex {
+public class ChinabondBondIndex {
 
 }
--- src/main/java/com/jstarcraft/crawler/trade/index/shfe/ShfeCommodityIndex.java
+++ src/main/java/com/jstarcraft/crawler/trade/index/shfe/ShfeCommodityIndex.java
@@ -3,11 +3,11 @@ package com.jstarcraft.crawler.trade.index.shfe;
 /**
  * 上海期货交易所指数
  * 
- * 指数行情:http://www.shfe.com.cn/bourseService/indexopt/index/market/
+ * 指数行情(商品):http://www.shfe.com.cn/bourseService/indexopt/index/market/
  * 
  * @author Birdy
  *
  */
-public interface ShfeIndex {
+public interface ShfeCommodityIndex {
 
 }
