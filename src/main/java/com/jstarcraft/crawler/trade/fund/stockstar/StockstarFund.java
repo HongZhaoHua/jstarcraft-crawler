d289297e31db1ba26543289a622badbfecb16d8f
Birdy <Birdy@192.168.2.60>
2022-04-12 17:39:25 +0800

    成立日/存续期页面.

--- src/main/java/com/jstarcraft/crawler/trade/fund/stockstar/StockstarFund.java
+++ src/main/java/com/jstarcraft/crawler/trade/fund/stockstar/StockstarFund.java
@@ -7,7 +7,7 @@ package com.jstarcraft.crawler.trade.fund.stockstar;
  * 开放式基金列表页面:http://quote.stockstar.com/fund/
  * 封闭式基金列表页面(折溢价率):http://quote.stockstar.com/fund/
  * 
- * 存续期页面:https://fund.eastmoney.com/{code}.html
+ * 成立日/存续期页面:https://fund.eastmoney.com/{code}.html
  * 
  * @author Birdy
  *
