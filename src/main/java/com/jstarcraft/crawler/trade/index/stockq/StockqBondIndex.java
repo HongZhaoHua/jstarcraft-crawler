791a81a5d532b5c44fe05ecc3064d83e796b108d
Birdy <Birdy@192.168.2.60>
2022-04-12 20:00:49 +0800

    JStarCraft规划.

--- src/main/java/com/jstarcraft/crawler/trade/index/stockq/StockqBondIndex.java
+++ src/main/java/com/jstarcraft/crawler/trade/index/stockq/StockqBondIndex.java
@@ -3,7 +3,9 @@ package com.jstarcraft.crawler.trade.index.stockq;
 /**
  * StockQ债券指数
  * 
- * 全球债券指数: https://www.stockq.org/bond/bondindex.php
+ * 全球债券指数:
+ * 
+ * https://www.stockq.org/bond/bondindex.php
  * https://www.stockq.org/bond/bondindex2.php
  * 
  * @author Birdy
