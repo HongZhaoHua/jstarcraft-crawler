db87bfd9252ab42a8c6e5e3b878c07a89fd15766
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-30 01:40:38 +0800

    乐咕乐股指数.

--- src/main/java/com/jstarcraft/crawler/trade/index/legulegu/LeguleguStockIndex.java
+++ src/main/java/com/jstarcraft/crawler/trade/index/legulegu/LeguleguStockIndex.java
@@ -9,7 +9,9 @@ package com.jstarcraft.crawler.trade.index.legulegu;
  * 指数估值页面:https://legulegu.com/stockdata/{code}-{ttm-lyr,pb}
  * 
  * A股股息率统计数据历史数据:https://legulegu.com/stockdata/guxilv
- * 中证行业/申万行业:https://legulegu.com/stockdata
+ * 中证行业指数列表页面:https://legulegu.com/stockdata/industry
+ * 申万行业指数列表页面:https://legulegu.com/stockdata/sw-industry-overview
+ * 规模、主题、策略、雪球指数列表页面:https://legulegu.com/stockdata/index-basic-overview
  * 
  * @author Birdy
  *
