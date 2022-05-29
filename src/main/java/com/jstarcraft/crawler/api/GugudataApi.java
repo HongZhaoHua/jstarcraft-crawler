94e9944350ac9ca84ef683957f7990b382dd8789
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-05-29 23:10:46 +0800

    快代理.

---
+++ src/main/java/com/jstarcraft/crawler/api/GugudataApi.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.crawler.api;
+
+/**
+ * 咕咕数据
+ * 
+ * https://www.gugudata.com/
+ * 
+ * @author Birdy
+ *
+ */
+public class GugudataApi {
+
+}
---
+++ src/main/java/com/jstarcraft/crawler/proxy/KuaidailiProxy.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.crawler.proxy;
+
+/**
+ * 快代理
+ * 
+ * https://free.kuaidaili.com/free/
+ * 
+ * @author Birdy
+ *
+ */
+public class KuaidailiProxy {
+
+}
