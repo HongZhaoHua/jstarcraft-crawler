7182a1604063866bf6a802f866025a2a39ac9599
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-05-29 17:32:54 +0800

    考试.

---
+++ src/main/java/com/jstarcraft/crawler/examination/Exam8Examination.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.crawler.examination;
+
+/**
+ * 考试吧考试
+ * 
+ * https://www.exam8.com/kaoshishijian/
+ * 
+ * @author Birdy
+ *
+ */
+public class Exam8Examination {
+
+}
---
+++ src/main/java/com/jstarcraft/crawler/examination/Test51Examination.java
@@ -0,0 +1,13 @@
+package com.jstarcraft.crawler.examination;
+
+/**
+ * 无忧考网考试
+ * 
+ * https://www.51test.net/kaoshirili/
+ * 
+ * @author Birdy
+ *
+ */
+public class Test51Examination {
+
+}
