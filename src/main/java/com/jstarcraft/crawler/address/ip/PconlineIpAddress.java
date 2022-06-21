eaba8f46d1c704725c8cd62fd47a2cd6d12b233c
Birdy <Birdy@LAPTOP-R44M770M>
2022-06-21 19:12:29 +0800

    太平洋网络.

---
+++ src/main/java/com/jstarcraft/crawler/address/ip/PconlineIpAddress.java
@@ -0,0 +1,11 @@
+package com.jstarcraft.crawler.address.ip;
+
+/**
+ * http://whois.pconline.com.cn/
+ * 
+ * @author Birdy
+ *
+ */
+public class PconlineIpAddress {
+
+}
--- src/test/java/com/jstarcraft/crawler/ip/IpTestCase.java
+++ src/test/java/com/jstarcraft/crawler/ip/IpTestCase.java
@@ -1,6 +1,11 @@
 package com.jstarcraft.crawler.ip;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.junit.Test;
@@ -80,8 +85,31 @@ public class IpTestCase {
         String url = StringUtility.format(ipUrl, ip);
         ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
         String data = response.getBody();
-        // Library for serializing Java objects into the PHP serializing format and unserializing data from this format back into Java objects.
+        // Library for serializing Java objects into the PHP serializing format and
+        // unserializing data from this format back into Java objects.
         // https://github.com/kayahr/pherialize
+    }
+
+    @Test
+    public void testPconline() {
+        RestTemplate template = new RestTemplate();
+        List<HttpMessageConverter<?>> converters = template.getMessageConverters();
+        for (HttpMessageConverter<?> converter : converters) {
+            // TODO StringHttpMessageConverter默认字符集为ISO_8859_1,重置字符集为GBK
+            if (converter instanceof StringHttpMessageConverter) {
+                StringHttpMessageConverter messageConverter = (StringHttpMessageConverter) converter;
+                messageConverter.setDefaultCharset(Charset.forName("GBK"));
+            }
+        }
+        String ip = "119.131.76.71";
+        String ipUrl = "http://whois.pconline.com.cn/ipJson.jsp?json=true&ip={}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.USER_AGENT, "PostmanRuntime/7.28.0");
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(null, headers);
+        String url = StringUtility.format(ipUrl, ip);
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+        String data = response.getBody();
+        System.out.println(JsonUtility.prettyJson(data));
     }
 
 }
