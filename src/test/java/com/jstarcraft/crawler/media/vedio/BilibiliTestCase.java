741cd4460b355d850a81b045bbf6f3abbf2d6ff2
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-12-11 12:07:24 +0800

    重构B站爬虫.

--- src/test/java/com/jstarcraft/crawler/media/vedio/BilibiliTestCase.java
+++ src/test/java/com/jstarcraft/crawler/media/vedio/BilibiliTestCase.java
@@ -1,24 +1,18 @@
 package com.jstarcraft.crawler.media.vedio;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.hamcrest.CoreMatchers;
 import org.jaxen.Navigator;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.junit.Assert;
 import org.junit.Test;
 import org.noear.snack.ONode;
 import org.slf4j.Logger;
